4a0894ba4cef66159d4f9b9836dfc280bbce6df4
Cami <Cami@gmail.com>
2023-10-17 21:13:55 -0300

    actividad-5-cambios

---
+++ src/components/Menu.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+const Menu = () => {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Inicio</Link>
+                </li>
+                <li>
+                    <Link to="/encuesta/crear">Crear Encuesta</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+export default Menu;
+        
